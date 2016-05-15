b7a8b203677930082a5db0985a62381e5be949a5
Carlos M. Vicente Herrero <Marlos@MacBook-Pro-de-Carlos.local>
2016-05-15 13:03:56 +0200

    Initial commit and created structure folder

---
+++ Contacts/ViewControllers/ContactListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Carlos M. Vicente Herrero on 15/5/16.
+//  Copyright © 2016 Carlos M. Vicente Herrero. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
